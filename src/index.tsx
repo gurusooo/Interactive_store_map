c152fbf672804667bf7039d30f5af3a5ae9de5d4
gurusooo <130143477+gurusooo@users.noreply.github.com>
2025-10-05 12:15:45 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import { defaultSystem } from "@chakra-ui/react"
+import { defaultSystem } from "@chakra-ui/react";
+import './global.css';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ChakraProvider value={defaultSystem}>
-        <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ChakraProvider>
         </React.StrictMode>,
 );
