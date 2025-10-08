8d58822e211b84e01e9a8e032fe1d2abf2dda156
gurusooo <130143477+gurusooo@users.noreply.github.com>
2025-10-08 04:33:01 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,72 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                "short_name": "Yummies' Cart",
+                "name": "Yummies' Cart",
+                "description": "Собирай корзину быстрее!",
+                "icons": [
+                    {
+                        "src": "icons/icon-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "any maskable"
+                    },
+                    {
+                        "src": "icons/icon-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any maskable"
+                    }
+                ],
+                "start_url": "/",
+                "background_color": "#f08c1f",
+                "display": "standalone",
+                "scope": "/",
+                "theme_color": "#f08c1f",
+                "orientation": "portrait-primary"
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    }
+                ]
+            },
+            devOptions: {
+                enabled: true,
+                type: 'module',
+                navigateFallback: 'index.html'
+            }
+        })
+    ],
     server: {
         port: 3000,
         open: true,
         host: true,
         watch: {
-            usePolling: true, 
-            interval: 100 
+            usePolling: true,
+            interval: 100
         }
     },
     css: {
