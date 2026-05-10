35d93fdb070ef3c81af60091f8fa07c481c8a0dd
gurusooo <mshkuleva@gmail.com>
2026-05-10 17:07:26 +0300

    ui/ux improvement: back button and minor changes

--- src/components/catalog/SearchHeader.tsx
+++ src/components/catalog/SearchHeader.tsx
@@ -1,5 +1,4 @@
-import { HStack } from "@chakra-ui/react";
-//import { FaShoppingCart } from "react-icons/fa";
+import { HStack, Heading, VStack, Box } from "@chakra-ui/react";
 import cartIcon from "../../assets/cart.svg";
 import styles from "./SearchHeader.module.css";
 import { useNavigate } from "react-router-dom";
@@ -9,12 +8,20 @@ export function SearchHeader() {
     const navigate = useNavigate();
 
     return (
-        <HStack className={styles.header}>
-            <button onClick={() => navigate("/")} className={styles.cartWrapper}>
-                <img src={cartIcon} alt="cart" className={styles.cartIcon} />
-            </button>
+        <VStack className={styles.container} gap={4} align="stretch">
+            <Box className={styles.titleWrapper}>
+                <Heading as="h1" size="lg" className={styles.h1}>
+                    Каталог
+                </Heading>
+            </Box>
 
-            <SearchWithSuggestions />
-        </HStack>
+            <HStack className={styles.header} justifyContent="space-between">
+                <button onClick={() => navigate("/")} className={styles.cartWrapper}>
+                    <img src={cartIcon} alt="cart" className={styles.cartIcon} />
+                </button>
+
+                <SearchWithSuggestions />
+            </HStack>
+        </VStack>
     );
 }
